fb2924928b06ddf7e9ce0f8cc80f2aa5dbe2fb38
Ibekason Alexander <alibekason@gmail.com>
2021-06-17 16:24:37 +0100

    Add form button sheet for login and signup

---
+++ app/lib/presentation/auth/form_bottom_sheet.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+class FormBottomSheet extends StatelessWidget {
+  final String title;
+  final Function onButtonPressed;
+  final Widget form;
+
+  const FormBottomSheet({Key key, this.title, this.onButtonPressed, this.form})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var mQ = MediaQuery.of(context).size;
+    return Container(
+      height: mQ.height * 0.9,
+      child: Column(
+        children: [
+          Container(
+            height: kToolbarHeight,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                CloseButton(
+                  color: Theme.of(context).primaryColor,
+                  onPressed: () => Navigator.pop(context),
+                ),
+                Text(
+                  title,
+                  style: TextStyle(
+                      color: Colors.grey,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18),
+                ),
+                SizedBox(
+                  width: 40,
+                  height: 40,
+                )
+              ],
+            ),
+          ),
+          Divider(
+            color: Colors.grey.shade300,
+            height: 0.5,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12),
+            child: Column(
+              children: [
+                Container(
+                  height: 60,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Myschool",
+                          style: TextStyle(
+                              color: Theme.of(context).primaryColor,
+                              fontSize: 40,
+                              fontWeight: FontWeight.w800)),
+                      Container(
+                        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: Text(
+                          title,
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.symmetric(vertical: 10),
+                        height: kToolbarHeight,
+                        child: Text(
+                          "$title on MySchool",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20),
+                        ),
+                      ),
+                      form ?? Container(),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
