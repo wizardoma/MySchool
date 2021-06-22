43b25c58d6bad75fffc386f077238a3f227ba4cb
Ibekason Alexander <alibekason@gmail.com>
2021-06-22 21:08:03 +0100

    add authentication client abstraction

---
+++ app/lib/domain/auth/authentication_client.dart
@@ -0,0 +1,18 @@
+import 'package:app/application/auth/login_request.dart';
+import 'package:app/application/auth/signup_request.dart';
+import 'package:app/domain/response.dart';
+
+abstract class AuthenticationClient {
+  Future<ResponseEntity> signUp(SignUpRequest signUpRequest);
+
+  Future<ResponseEntity> login(LoginRequest request);
+
+  Future<ResponseEntity> loginWithGoogle();
+
+  // delete newly created user
+  Future<void> delete();
+
+  void logout();
+
+  Future<ResponseEntity> checkAuthentication();
+}
