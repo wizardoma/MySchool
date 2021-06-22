d2b55b87dfa09eba7293784335430ca65472bfcf
Ibekason Alexander <alibekason@gmail.com>
2021-06-22 21:13:51 +0100

    implement logging out

--- app/lib/presentation/widgets/drawer/drawer.dart
+++ app/lib/presentation/widgets/drawer/drawer.dart
@@ -1,4 +1,8 @@
+import 'package:app/application/auth/auth_bloc.dart';
+import 'package:app/application/auth/auth_event.dart';
+import 'package:app/application/auth/auth_state.dart';
 import 'package:app/application/theme/theme_cubit.dart';
+import 'package:app/presentation/auth/auth_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -35,88 +39,98 @@ class HomeDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var mQ = MediaQuery.of(context).size;
+
     return SafeArea(
-      child: Drawer(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              children: [
-                Container(
-                  child: DrawerHeader(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        CircleAvatar(
-                          radius: 40,
-                          backgroundImage:
-                              AssetImage("assets/icons/student.png"),
-                        ),
-                        ListTile(
-                          title: Text(
-                            "Alexander Ibekason",
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18,
-                            ),
-                          ),
-                          trailing: Icon(
-                            Icons.arrow_forward_ios,
-                            size: 20,
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-                Container(
-                  child: ListView(
-                    shrinkWrap: true,
-                    children: _items.map((e) {
-                      return ListTile(
-                        leading: Icon(e["icon"]),
-                        title: Text(e["title"]),
-                      );
-                    }).toList(),
-                  ),
-                )
-              ],
-            ),
-            Container(
-              padding: EdgeInsets.all(5),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      child: BlocListener<AuthenticationBloc, AuthenticationState>(
+        listener: (context,state) {
+          if (state is NotAuthenticatedState) {
+            Future.delayed(Duration(seconds: 2), () {
+              Navigator.pushNamedAndRemoveUntil(context, AuthScreen.routeName, (route) => false);
+            });
+          }
+        },
+        child: Drawer(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
                 children: [
                   Container(
-                    child: Row(
-                      children: [
-                        Icon(Icons.settings_outlined),
-                        SizedBox(width: 5,),
-                        Text("Settings"),
-                      ],
+
+                    child: DrawerHeader(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          CircleAvatar(
+                            radius: 40,
+                            backgroundImage:
+                                AssetImage("assets/icons/student.png"),
+                          ),
+                          ListTile(
+                            title: Text(
+                              "Alexander Ibekason",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                              ),
+                            ),
+                            trailing: Icon(
+                              Icons.arrow_forward_ios,
+                              size: 20,
+                            ),
+                          )
+                        ],
+                      ),
                     ),
                   ),
                   Container(
-                    child: Row(
-                      children: [
-                        IconButton(
-                          icon: Icon(Icons.lightbulb, color: Colors.black54,),
-                          onPressed: () {
-                            BlocProvider.of<ThemeCubit>(context).setTheme();
-                          },
-                        ),
-                        IconButton(
-                            icon: Icon(Icons.more_horiz, color: Colors.black54,), onPressed: null),
-                      ],
+                    child: ListView(
+                      shrinkWrap: true,
+                      children: _items.map((e) {
+                        return ListTile(
+                          leading: Icon(e["icon"]),
+                          title: Text(e["title"]),
+                        );
+                      }).toList(),
                     ),
-                  ),
+                  )
                 ],
               ),
-            )
-          ],
+              Container(
+                padding: EdgeInsets.all(5),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      child: Row(
+                        children: [
+                          Icon(Icons.settings_outlined),
+                          SizedBox(width: 5,),
+                          Text("Settings"),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      child: Row(
+                        children: [
+                          IconButton(
+                            icon: Icon(Icons.lightbulb, color: Colors.black54,),
+                            onPressed: () {
+                              BlocProvider.of<ThemeCubit>(context).setTheme();
+                            },
+                          ),
+                          IconButton(
+                              icon: Icon(Icons.logout, color: Colors.black54,), onPressed:() => context.read<AuthenticationBloc>().add(LogoutEvent())),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
