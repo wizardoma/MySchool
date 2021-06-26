3b3bfdf8379403e50f514d690cef74f8f4200e32
Ibekason Alexander <alibekason@gmail.com>
2021-06-26 21:36:39 +0100

    add user client

---
+++ app/lib/domain/user/user_client.dart
@@ -0,0 +1,14 @@
+
+
+import 'package:app/domain/user/user.dart';
+
+class UserClient {
+  final User currentUser = User(
+    id: "sfsff",
+    email: "alibeson@gmail.com",
+    department: "Computer science",
+    university: "futo",
+    level: "500L",
+  );
+
+}
