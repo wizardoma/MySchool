0ee558acad2497dfe594445d05536d0d1b0b627c
Ibekason Alexander <alibekason@gmail.com>
2021-07-14 19:51:30 +0100

    Add ui_helper widgets

---
+++ app/lib/commons/ui_helpers.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+// horizontal spacing
+const Widget kHorizontalSpaceTiny = SizedBox(width: 5.0);
+const Widget kHorizontalSpaceSmall = SizedBox(width: 10.0);
+const Widget kHorizontalSpaceRegular = SizedBox(width: 18.0);
+const Widget kHorizontalSpaceMedium = SizedBox(width: 25.0);
+const Widget kHorizontalSpaceLarge = SizedBox(width: 50.0);
+
+// vertical spacing
+const Widget kVerticalSpaceTiny = SizedBox(height: 5.0);
+const Widget kVerticalSpaceSmall = SizedBox(height: 10.0);
+const Widget kVerticalSpaceRegular = SizedBox(height: 18.0);
+const Widget kVerticalSpaceMedium = SizedBox(height: 25.0);
+const Widget kVerticalSpaceLarge = SizedBox(height: 50.0);
