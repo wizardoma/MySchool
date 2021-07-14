dc67c575bf049fd48855fac6eb2b78c2d733c7e4
Ibekason Alexander <alibekason@gmail.com>
2021-07-14 19:51:57 +0100

    Add central styles

---
+++ app/lib/commons/styles.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const kAuthorNameStyle = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 16
+);
+
+const kPostTitleTextStyle = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.bold,
+);
