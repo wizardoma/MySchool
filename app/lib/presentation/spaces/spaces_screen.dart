67d0321fc12b732fa9301d8b329c15ece18a43ad
Ibekason Alexander <alibekason@gmail.com>
2021-07-15 21:16:57 +0100

    refractor: improve space screen design

--- app/lib/presentation/spaces/spaces_screen.dart
+++ app/lib/presentation/spaces/spaces_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/widgets.dart';
 
 class SpacesScreen extends StatefulWidget {
   static const title = "Spaces";
+
   SpacesScreen({Key key}) : super(key: key);
 
   @override
@@ -13,18 +14,18 @@ class SpacesScreen extends StatefulWidget {
 class _SpacesScreenState extends State<SpacesScreen> {
   final List<String> _spaces = [
     "Computer Science",
-    "Electrical and Electronics Engineering"
-    ,
+    "Electrical and Electronics Engineering",
     "Pharmacy",
     "Medical Science",
     "Mechatronics",
     "MicroBiology",
     "Agricultural Engineering",
-    "Chemistry","Physics",
+    "Chemistry",
+    "Physics",
     "Political Science",
     "Architecture",
-
   ];
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -37,39 +38,21 @@ class _SpacesScreenState extends State<SpacesScreen> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Container(
-                  child: Text("Your Spaces", style: TextStyle(
-                    color: Colors.black87,
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
-                  ),),
+                  child: Text(
+                    "Your Spaces",
+                    style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
                 kVerticalSpaceSmall,
                 Row(
                   children: [
                     Container(
-                      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.blue),
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Row(
-                        children: [
-                          Container(
-                            padding: EdgeInsets.all(0),
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              border: Border.all(color: Colors.blue)
-                            ),
-                            child: Icon(Icons.add, color: Colors.blue, ),
-                          ),
-                          kHorizontalSpaceTiny,
-                          Text("Create", style: TextStyle(color: Colors.blue),)
-                        ],
-                      ),
-                    ),
-                    kHorizontalSpaceMedium,
-                    Container(
-                      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 10, vertical: 5),
                       decoration: BoxDecoration(
                         border: Border.all(color: Colors.blue),
                         borderRadius: BorderRadius.circular(20),
@@ -80,17 +63,50 @@ class _SpacesScreenState extends State<SpacesScreen> {
                             padding: EdgeInsets.all(0),
                             decoration: BoxDecoration(
                                 shape: BoxShape.circle,
-                                border: Border.all(color: Colors.blue)
+                                border: Border.all(color: Colors.blue)),
+                            child: Icon(
+                              Icons.add,
+                              size: 15,
+                              color: Colors.blue,
                             ),
-                            child: Icon(Icons.search, color: Colors.blue, ),
                           ),
                           kHorizontalSpaceTiny,
-                          Text("Discover", style: TextStyle(color: Colors.blue),)
+                          Text(
+                            "Create",
+                            style: TextStyle(color: Colors.blue),
+                          ),
                         ],
                       ),
                     ),
-
-
+                    kHorizontalSpaceSmall,
+                    Container(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.blue),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Row(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.all(0),
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                border: Border.all(color: Colors.blue)),
+                            child: Icon(
+                              Icons.search,
+                              size: 15,
+                              color: Colors.blue,
+                            ),
+                          ),
+                          kHorizontalSpaceTiny,
+                          Text(
+                            "Discover",
+                            style: TextStyle(color: Colors.blue),
+                          )
+                        ],
+                      ),
+                    ),
                   ],
                 )
               ],
@@ -98,22 +114,61 @@ class _SpacesScreenState extends State<SpacesScreen> {
           ),
           kVerticalSpaceSmall,
           Expanded(
-            child: ListView(
+            child: ListView.separated(
               physics: ScrollPhysics(),
               shrinkWrap: true,
-              children: _spaces.map((space) => ListTile(
-                leading: Image.asset("assets/images/group_task.png", width:20, height:20),
-                title: Text(space, style: TextStyle(
-                  fontWeight: FontWeight.bold,
-
-                ),
-                ),
-                trailing: Icon(Icons.arrow_forward_ios),
-              )).toList(),
+              separatorBuilder: (context, index) => Divider(color: Colors.grey.shade200, thickness: 0.5,),
+              itemBuilder: (context, index)=>  CustomListTile(
+                        leading: Image.asset("assets/images/group_task.png",
+                            width: 20, height: 20),
+                        title: Text(
+                          _spaces[index],
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15
+                          ),
+                        ),
+                        trailing: Icon(Icons.arrow_forward_ios),
+                      ), itemCount: _spaces.length,)
+                  ,
             ),
-          )
+          ]
+      )
+
+
+    );
+  }
+}
+
+class CustomListTile extends StatelessWidget {
+  final Widget leading;
+  final Widget trailing;
+  final Widget title;
+
+  const CustomListTile({Key key, this.leading, this.trailing, this.title}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            child: Row(
+              children: [
+                leading,
+                kHorizontalSpaceSmall,
+                title,
+              ],
+            ),
+          ),
+          Container(
+            child: Icon(Icons.arrow_forward_ios, size: 15, color: Colors.grey,),
+          ),
+
         ],
       ),
     );
   }
 }
+
