9527a54e972691b240dc2613da09527ed8283729
Ibekason Alexander <alibekason@gmail.com>
2021-07-16 09:45:00 +0100

    add random list of spaces

---
+++ app/lib/domain/space/spaces_list.dart
@@ -0,0 +1,13 @@
+final List<String> spacesList = [
+  "Computer Science",
+  "Electrical and Electronics Engineering",
+  "Pharmacy",
+  "Medical Science",
+  "Mechatronics",
+  "MicroBiology",
+  "Agricultural Engineering",
+  "Chemistry",
+  "Physics",
+  "Political Science",
+  "Architecture",
+];
