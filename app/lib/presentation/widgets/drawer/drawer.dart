64d5c905e2f527fc577e95548069f0e2dd1608b5
Ibekason Alexander <alibekason@gmail.com>
2021-07-19 16:33:13 +0100

    add shimmer loader to user view in drawer

--- app/lib/presentation/widgets/drawer/drawer.dart
+++ app/lib/presentation/widgets/drawer/drawer.dart
@@ -2,11 +2,15 @@ import 'package:app/application/auth/auth_bloc.dart';
 import 'package:app/application/auth/auth_event.dart';
 import 'package:app/application/auth/auth_state.dart';
 import 'package:app/application/theme/theme_cubit.dart';
+import 'package:app/application/user/user_bloc.dart';
+import 'package:app/application/user/user_state.dart';
+import 'package:app/commons/ui_helpers.dart';
 import 'package:app/presentation/auth/auth_screen.dart';
 import 'package:app/presentation/profile/profile_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shimmer/shimmer.dart';
 
 class HomeDrawer extends StatelessWidget {
   final _items = [
@@ -51,9 +55,57 @@ class HomeDrawer extends StatelessWidget {
             children: [
               Column(
                 children: [
-                  Container(
-                    child: DrawerHeader(
-                      child: Column(
+                  Container(child:
+                      DrawerHeader(child: BlocBuilder<UserBloc, UserState>(
+                          // ignore: missing_return
+                          builder: (context, state) {
+                    if (state is UserNotInitializedState ||
+                        state is FetchingUserState) {
+                      return Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Shimmer(
+                              child: CircleAvatar(
+                                radius: 40,
+                              ),
+                              gradient: LinearGradient(
+                                colors: [Colors.grey, Colors.grey.shade300],
+                              ),
+                            ),
+                            kVerticalSpaceSmall,
+                            Shimmer(
+                              child: Container(
+                                height: 30,
+                                width: double.infinity,
+                                child: LayoutBuilder(
+                                  builder: (c, cn) => Row(
+//                                    mainAxisAlignment:
+//                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Container(
+                                        height: 30,
+                                        width: cn.maxWidth * 0.4,
+                                        color: Colors.grey,
+                                      ),
+                                      kHorizontalSpaceTiny,
+                                      Container(
+                                        height: 30,
+                                        width: cn.maxWidth * 0.55,
+
+                                        color: Colors.grey,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+//                              color: Colors.grey,
+                              ),
+                              gradient: LinearGradient(
+                                  colors: [Colors.grey, Colors.grey.shade300]),
+                            )
+                          ]);
+                    }
+                    if (state is FetchedUserState) {
+                      return Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           CircleAvatar(
@@ -78,9 +130,9 @@ class HomeDrawer extends StatelessWidget {
                             ),
                           ),
                         ],
-                      ),
-                    ),
-                  ),
+                      );
+                    }
+                  }))),
                   Container(
                     child: ListView(
                       shrinkWrap: true,
