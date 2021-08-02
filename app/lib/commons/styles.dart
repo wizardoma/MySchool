243dede66a6dbbe39853d36631c725a0988e86dd
Ibekason Alexander <alibekason@gmail.com>
2021-08-02 18:11:58 +0100

    fix: copy existing style instead of overriding

--- app/lib/commons/styles.dart
+++ app/lib/commons/styles.dart
@@ -8,19 +8,31 @@ const kPostTitleTextStyle =
 const kSubtitleTextStyle = TextStyle(fontSize: 12, color: Colors.grey);
 
 ThemeData customTheme(bool isDark) {
-  return isDark
-      ? ThemeData.dark()
-      : ThemeData.light().copyWith(
-          scaffoldBackgroundColor: Colors.white,
-          textTheme: TextTheme(
-            bodyText2: TextStyle(
-              color: Colors.black87,
-              fontSize: 15,
-            ),
-            bodyText1: TextStyle(
-              color: Colors.black87,
-              fontSize: 15,
-            ),
+  var theme = isDark ? ThemeData.dark() : ThemeData.light();
+  return getTheme(theme);
+}
+
+ThemeData getTheme(ThemeData themeData) {
+  return themeData.copyWith(
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: themeData.textTheme
+        .copyWith(
+          bodyText2: themeData.textTheme.bodyText2.copyWith(
+            color: Colors.black87,
+            fontSize: 15,
           ),
-        );
+          bodyText1: themeData.textTheme.bodyText1.copyWith(
+            color: Colors.black87,
+            fontSize: 15,
+          ),
+          headline6: themeData.textTheme.headline6.copyWith(
+            color: Colors.black87,
+            fontSize: 15,
+          ),
+        )
+        .apply(
+          bodyColor: Colors.black87,
+          displayColor: Colors.black87,
+        ),
+  );
 }
