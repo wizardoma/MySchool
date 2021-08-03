a09cfa9cc544f3e9e08aa32b8dda195b39779a00
Ibekason Alexander <alibekason@gmail.com>
2021-08-03 22:21:05 +0100

    register student on server on signup

--- app/lib/domain/auth/authentication_service_impl.dart
+++ app/lib/domain/auth/authentication_service_impl.dart
@@ -5,6 +5,7 @@ import 'package:app/domain/auth/authentication_service.dart';
 import 'package:app/domain/auth/model/authUser.dart';
 import 'package:app/domain/auth/model/signUpUser.dart';
 import 'package:app/domain/response.dart';
+import 'package:dio/dio.dart';
 import 'package:http_code_util/http_code_util.dart';
 
 /// Author: Ibekason Alexander
@@ -42,44 +43,33 @@ class AuthenticationServiceImpl extends AuthenticationService
     var response = await authenticationClient.signUp(signUpRequest);
     if (response.isError) return response;
     var userData = SignUpUser(
-        id: (response.data as AuthUser).id,
-        email: signUpRequest.email,
-        department: signUpRequest.department,
-        matricNo: signUpRequest.matricNo,
-        level: signUpRequest.level,
-        university: signUpRequest.university,
-        name: signUpRequest.name);
-    return ResponseEntity.Data(userData);
+            id: (response.data as AuthUser).id,
+            email: signUpRequest.email,
+            department: signUpRequest.department,
+            matricNo: signUpRequest.matricNo,
+            level: signUpRequest.level,
+            university: signUpRequest.university,
+            name: signUpRequest.name)
+        ;
 
-//    if (authenticationClient is FirebaseAuthService){
-//      try {
-//        var userData = SignUpUser(
-//              id: (response.data as AuthUser).id,
-//              email: signUpRequest.email,
-//              department: signUpRequest.department,
-//              level: signUpRequest.level,
-//              university: signUpRequest.university,
-//              name: signUpRequest.name
-//            ).toMap();
-//        print(userData);
-//        await Dio(BaseOptions(
-//          connectTimeout: 15000,receiveTimeout: 15000
-//        )).post("https://myschool-project.herokuapp.com/api/v1/students",
-//            data: FormData.fromMap(userData));
-//        return response;
-//      }
-//      on DioError catch(e) {
-//        authenticationClient.delete();
-//        print("DioError: ${e.error}");
-//        return ResponseEntity.Error("An error occurred trying to register");
-//      }
-//
-//      catch (e) {
-//        authenticationClient.delete();
-//        print("Exception ${e.response}");
-//        return ResponseEntity.Error("An error occurred trying to register");
-//
-//      }
-//    }
+    Response serverResponse;
+    try {
+      print(" userdata $userData");
+      serverResponse =
+          await Dio(BaseOptions(connectTimeout: 15000, receiveTimeout: 15000))
+              .post("https://myschool-project.herokuapp.com/api/v1/students",
+                  data: FormData.fromMap(userData.toMap()));
+          return ResponseEntity.Data(userData);
+
+    } on DioError catch (e) {
+      authenticationClient.delete();
+      print("DioError: ${e.error}");
+      var errorSummary = "An error occurred trying to register";
+      return ResponseEntity.Error(errorSummary);
+    } catch (e) {
+      authenticationClient.delete();
+      print("Exception ${e.response}");
+      return ResponseEntity.Error("An error occurred trying to register");
+    }
   }
 }
