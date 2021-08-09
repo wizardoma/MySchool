34d087587663749d06890e2969927663592c8edd
Ibekason Alexander <alibekason@gmail.com>
2021-08-09 12:45:37 +0100

    Add spaces initializer

---
+++ backend/src/main/java/com/wizardom/backend/domain/space/config/SpaceInitializer.java
@@ -0,0 +1,89 @@
+package com.wizardom.backend.domain.space.config;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SpaceInitializer {
+
+    public static Map<String, List<Integer>> defaultSpaces ;
+
+    static {
+        defaultSpaces = new HashMap<>();
+        defaultSpaces.put("Accountancy", Arrays.asList(68,51,52,133));
+        defaultSpaces.put("Agriculture", Arrays.asList(1,2,53,3,54,55));
+        defaultSpaces.put("Anatomy", Arrays.asList(4));
+        defaultSpaces.put("Animal Science", Arrays.asList(134,56,5));
+        defaultSpaces.put("Architecture", Arrays.asList(57,6));
+        defaultSpaces.put("Banking and Finance", Arrays.asList(58,135));
+        defaultSpaces.put("Biochemistry", Arrays.asList(136,59,7));
+        defaultSpaces.put("Biological Science", Arrays.asList(69,8,137));
+        defaultSpaces.put("Biomedical Technology", Arrays.asList(9,10));
+        defaultSpaces.put("Botany", Arrays.asList(60));
+        defaultSpaces.put("Building Technology", Arrays.asList(61,11));
+        defaultSpaces.put("Business Administration", Arrays.asList(138,85,86));
+        defaultSpaces.put("Chemical Science", Arrays.asList(12,62,13,139,63,70));
+        defaultSpaces.put("Civil Engineering", Arrays.asList(15,64));
+        defaultSpaces.put("Computer Science", Arrays.asList(140,16,65,45));
+        defaultSpaces.put("Plant and Crop Science Technology", Arrays.asList(66,153,17));
+        defaultSpaces.put("Cyber Security", Arrays.asList(18));
+        defaultSpaces.put("Dental Technology", Arrays.asList(19));
+        defaultSpaces.put("Economics", Arrays.asList(72,141,67));
+        defaultSpaces.put("Psychology and Counselling", Arrays.asList(84,96));
+        defaultSpaces.put("Religious Studies", Arrays.asList(71,83,122,157));
+        defaultSpaces.put("Linguistics", Arrays.asList(73,75,80,88,146,107,142,93,100));
+        defaultSpaces.put("Geography", Arrays.asList(76,77,94));
+        defaultSpaces.put("History", Arrays.asList(79,144,97));
+        defaultSpaces.put("Physics", Arrays.asList(82,103,151,38,118,152));
+        defaultSpaces.put("Mathematics", Arrays.asList(181,101,32,148,111));
+        defaultSpaces.put("Fine and Applied Arts", Arrays.asList(74,132,90));
+        defaultSpaces.put("Electrical and Electronics Engineering", Arrays.asList(20,87));
+        defaultSpaces.put("Entrepreneurship", Arrays.asList(143));
+        defaultSpaces.put("Environmental Technology", Arrays.asList(21));
+        defaultSpaces.put("Estate Management", Arrays.asList(89));
+        defaultSpaces.put("Finance", Arrays.asList(22));
+        defaultSpaces.put("Fishery", Arrays.asList(23));
+        defaultSpaces.put("Food Science and Technology", Arrays.asList(91,24));
+        defaultSpaces.put("Forestry and Wildlife", Arrays.asList(92,25));
+        defaultSpaces.put("Geology", Arrays.asList(26));
+        defaultSpaces.put("Public Administration", Arrays.asList(95,156));
+        defaultSpaces.put("Home Economics and Education", Arrays.asList(98));
+        defaultSpaces.put("Hospitality and Tourism", Arrays.asList(99));
+        defaultSpaces.put("Microbiology", Arrays.asList(102,35,114,149));
+        defaultSpaces.put("Relations and Personnel Management", Arrays.asList(145));
+        defaultSpaces.put("Information Technology", Arrays.asList(28,27));
+        defaultSpaces.put("Insurance ", Arrays.asList(104));
+        defaultSpaces.put("Land Surveying", Arrays.asList(105,131));
+        defaultSpaces.put("Library and Information Science", Arrays.asList(106));
+        defaultSpaces.put("Management Technology", Arrays.asList(108,29));
+        defaultSpaces.put("Maritime Management Technology", Arrays.asList(30));
+        defaultSpaces.put("Marketing", Arrays.asList(109,147));
+        defaultSpaces.put("Mass Communication", Arrays.asList(110));
+        defaultSpaces.put("Material and Metallurgical Engineering", Arrays.asList(31));
+        defaultSpaces.put("Mechanical Engineering", Arrays.asList(112,33,34));
+        defaultSpaces.put("Medical and Science Laboratory Technology", Arrays.asList(113,14));
+        defaultSpaces.put("Optometry", Arrays.asList(36));
+        defaultSpaces.put("Petroleum Engineering", Arrays.asList(37));
+        defaultSpaces.put("Philosophy", Arrays.asList(117,150));
+        defaultSpaces.put("Psychology", Arrays.asList(39,119,155));
+        defaultSpaces.put("Political Science", Arrays.asList(154));
+        defaultSpaces.put("Polymer and Textile", Arrays.asList(40));
+        defaultSpaces.put("Project Management", Arrays.asList(41));
+        defaultSpaces.put("Prosthesis and Orthopedics", Arrays.asList(42));
+        defaultSpaces.put("Public Health", Arrays.asList(120,43));
+        defaultSpaces.put("Quantity Surveying", Arrays.asList(121,44));
+        defaultSpaces.put("Sociology", Arrays.asList(123,158));
+        defaultSpaces.put("Soil Science", Arrays.asList(124,125,46));
+        defaultSpaces.put("Statistics", Arrays.asList(159,126,47));
+        defaultSpaces.put("Surveying and GeoInformatics", Arrays.asList(127,48));
+        defaultSpaces.put("Transport Management", Arrays.asList(49));
+        defaultSpaces.put("Urban and Regional Planning", Arrays.asList(129,50));
+        defaultSpaces.put("Zoology", Arrays.asList(130));
+
+    }
+
+
+
+
+}
